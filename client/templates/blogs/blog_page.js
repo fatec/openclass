a9230daac8d9dc0209181c1a5633909d65cba1c1
Vincent Widmer <eodecorp@hotmail.com>
2015-08-25 19:56:36 +0200

    Added a post counter

--- client/templates/blogs/blog_page.js
+++ client/templates/blogs/blog_page.js
@@ -1,5 +1,7 @@
 Template.blogPage.helpers({
   posts: function() {
     return Posts.find({blogId: this._id}, {sort: {submitted: -1}});
-  }
+  },
+  postCount: function() { // return the number of posts
+    return Posts.find().count();  }
 });
