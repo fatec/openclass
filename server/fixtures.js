d70130fc0eafd90413fe1ea10d4c7c0061ace848
Vincent Widmer <eodecorp@hotmail.com>
2015-08-28 12:00:00 +0200

    Edited default blog + added admin user

--- server/fixtures.js
+++ server/fixtures.js
@@ -11,42 +11,32 @@ if (Blogs.find().count() === 0) {
   });
   var vincent = Meteor.users.findOne(vincentId);
   
+  // Create admin user
+seedUserId = Accounts.createUser({
+    username: 'admin',
+    password: 'admin'
+  });
+
+  // Create example user
+  var exampleId = Meteor.users.insert({
+    profile: { name: 'bzzbox' }
+  });
+  var example = Meteor.users.findOne(exampleId);
+
   
   var firstBlogId = Blogs.insert({
-    title: 'Sortie au bout du monde',
-    userId: steph._id,
-    author: steph.profile.name,
+    title: 'Exemple de journal',
+    userId: example._id,
+    author: example.profile.name,
     submitted: new Date(now - 7 * 3600 * 1000),
   });
 
   Posts.insert({
     blogId: firstBlogId,
-    userId: steph._id,
-    author: steph.profile.name,
+    userId: example._id,
+    author: example.profile.name,
     submitted: new Date(now - 5 * 3600 * 1000),
-    body: 'Trop bien!'
-  });
-
-  Posts.insert({
-    blogId: firstBlogId,
-    userId: vincent._id,
-    author: vincent.profile.name,
-    submitted: new Date(now - 3 * 3600 * 1000),
-    body: 'Super!! Youpi'
-  });
-
-  Blogs.insert({
-    title: 'Sur le Salève',
-    userId: vincent._id,
-    author: vincent.profile.name,
-    submitted: new Date(now - 10 * 3600 * 1000),
-  });
-
-  Blogs.insert({
-    title: 'Les abeilles',
-    userId: vincent._id,
-    author: vincent.profile.name,
-    submitted: new Date(now - 12 * 3600 * 1000),
+    body: 'Voici votre première publication !'
   });
 
 }
