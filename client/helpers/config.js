602c4a22733c9e459fed67d5ae5f6f54375fc227
Vincent Widmer <eodecorp@hotmail.com>
2015-08-29 19:30:52 +0200

    Added localization for accounts:semantic-ui

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -6,3 +6,7 @@ Template.registerHelper('actualLoadedTemplate', function(){
     return Router.current() && Router.current().route.getName().replace('.','-');
   }
 );
+
+// Change semantic-ui localization with mrt:accounts-t9n 
+T9n.setLanguage("fr");
+dans helpers -> config.js
