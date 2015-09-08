70105f9123f7f3e4c47f49ac6ebb21d24b5a7616
Stephane Morand <stephane.morand@unige.ch>
2015-09-08 10:05:57 +0200

    tag display in menu

--- client/templates/posts/posts_menu.js
+++ client/templates/posts/posts_menu.js
@@ -1,4 +1,8 @@
 Template.postsMenu.helpers({
 	postCount: function() { // return the number of posts
-		return Posts.find().count();  }
+		return Posts.find().count();
+	},
+	tags: function() {
+		return Tags.find({}, {sort: {nRefs: -1}});
+	}
 });
