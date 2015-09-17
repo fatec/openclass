1ad61ba4b9d6562b9034c2d9f9f4b1794e7f5d22
Vincent Widmer <eodecorp@hotmail.com>
2015-09-17 17:02:45 +0200

    Removed old menus

--- client/templates/posts/desktop_menu.js
+++
@@ -1,8 +0,0 @@
-Template.desktopMenu2.helpers({
-	postCount: function() { // return the number of posts
-		return Posts.find().count();
-	},
-	tags: function() {
-		return Tags.find({}, {sort: {nRefs: -1}});
-	}
-});
