e08e29c2f6e452d22163af1ea6564c6f1c587a12
Stéphane Morand <stephane.morand@unige.ch>
2015-09-17 22:43:21 +0200

    first try to use server and write stream

--- server/methods.js
+++ server/methods.js
@@ -1,5 +1,38 @@
 Meteor.startup(function() {
 
+var net = Npm.require('net');
+var fs = Npm.require('fs');
+var buffer = Npm.require('buffer');
+
+var wstream = fs.createWriteStream("/Users/morands/Downloads/monimage.jpg");
+
+var server = net.createServer(function(conn) {
+    console.log('server connected');
+
+    conn.on('data', function(data) {
+        console.log('data received');
+        //console.log('data is: \n' + data);
+        wstream.write(data);
+    });
+});
+
+var HOST = '127.0.0.1';
+var PORT = '9001'
+var FILEPATH = '/home/morands/Downloads/';
+
+
+server.listen(PORT, HOST, function() {
+    //listening
+    console.log('server bound to ' + PORT + '\n');
+
+    server.on('connection', function(){
+        console.log('connection made...\n')
+    })
+});
+
+
+
+
     return Meteor.methods({
 
 		removeGhostImages: function() {
@@ -13,3 +46,5 @@ Meteor.startup(function() {
   		}
     });
 });
+
+
