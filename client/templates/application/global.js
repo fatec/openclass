545ad1f26c17fa07180b8ca68a473392ed7b68f0
Vincent Widmer <eodecorp@hotmail.com>
2015-09-24 15:16:59 +0200

    Removed old stuff

--- client/templates/application/global.js
+++
@@ -1,6 +0,0 @@
-UI.registerHelper('shortIt', function(stringToShorten, maxCharsAmount){
-  if(stringToShorten.length > maxCharsAmount){
-    return stringToShorten.substring(0, maxCharsAmount) + '...';
-  }
-  return stringToShorten;
-});
