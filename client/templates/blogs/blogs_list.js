8c9f2e116d0663057c697c3f9c927919750f07e0
Vincent Widmer <eodecorp@hotmail.com>
2015-10-13 19:32:12 +0200

    Added code access

--- client/templates/blogs/blogs_list.js
+++ client/templates/blogs/blogs_list.js
@@ -3,3 +3,21 @@ Template.blogsList.helpers({
 		return Blogs.find();
 	}
 });
+
+
+Template.blogsList.events({
+	'submit form.blogs-list--code-link-form': function(e) {
+		e.preventDefault();
+		var code = $(e.target).find('[id=code]').val();
+		console.log(Blogs.findOne(code));
+		if (Blogs.findOne(code))
+        	Router.go('blogPage', {_id: code});
+		else
+			alert("Ce journal n'existe pas.");
+},
+    'click .blogs-list--button-code-link': function(e) {
+    e.preventDefault();
+    $('.blogs-list--code-link-form').submit();
+  },
+
+});
