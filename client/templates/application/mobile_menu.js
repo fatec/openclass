a8272d4366b6efd26c7140c162add9f049976027
Vincent Widmer <eodecorp@hotmail.com>
2015-10-21 17:07:27 +0200

    Mobile menu

--- client/templates/application/mobile_menu.js
+++ client/templates/application/mobile_menu.js
@@ -4,5 +4,64 @@ Template.mobileMenu.helpers({
 	},
 	tags: function() {
 		return Tags.find({}, {sort: {nRefs: -1}});
-	}
+	},
+	categories: function() {
+		//return Categories.find({}, nRefs: { $gt: 0 }, {sort: {nRefs: -1}});
+				return Categories.find({ nRefs: { $gt: 0 } }, {sort: {nRefs: -1}});
+
+	},	
+	tagQuery: function() {
+		return "tags="+this.name;
+	},
+	categoryQuery: function() {
+		return "category="+this.name;
+	},	
+	'selectedTagClass': function(){
+		var tagId = this.name;
+	    var selectedTag = Session.get('selectedTag');
+	    if(tagId == selectedTag){
+	        return "menu--tag-selected"
+	    }
+	},
+	'selectedCategoryClass': function(){
+		var categoryId = this.name;
+	    var selectedCategory = Session.get('selectedCategory');
+	    if(categoryId == selectedCategory){
+	        return "menu--author-selected"
+	    }
+	},	
+	'selectedAllPostsClass': function(){
+	    var sortPosts = Session.get('sortPosts');
+	    if(sortPosts == "all"){
+	        return "menu--link-sort-selected"
+	    }
+	},
+	'selectedLastPostsClass': function(){
+    	var sortPosts = Session.get('sortPosts');
+    	if(sortPosts == "last"){
+        	return "menu--link-sort-selected"
+    	}
+	},
+	'selectedAuthorClass': function(){
+		var authorName = this.name;
+	    var selectedAuthor = Session.get('selectedAuthor');
+	    if(authorName == selectedAuthor){
+	        return "menu--author-selected"
+	    }
+	},	
+	authors: function() {
+		return Authors.find({ nRefs: { $gt: 0 } }, {sort: {name: 1}});
+	},
+	authorQuery: function() {
+		return "author="+this.name;
+	},	
+});
+
+  Template.mobileMenu.events({
+  'click .menu--link-last-posts': function(e) {
+    Session.set("sortPosts", "last");    
+  },
+  'click .menu--link-all-posts': function(e) {
+    Session.set("sortPosts", "all");    
+  }  
 });
