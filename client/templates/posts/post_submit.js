e7ef1d36ff06a485dce238bb79c33d43917a24a6
Stephane Morand <stephane.morand@unige.ch>
2015-10-23 12:20:04 +0200

    resize

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -42,12 +42,21 @@ Template.postSubmit.events({
     FS.Utility.eachFile(event, function(file) {
       var blogId = template.data.blog._id;
 
-      var newFile = new FS.File(file);
+      myCanvasFunction(file, function (image) {
+        var newFile = image;
+        console.log("newFile1 "+ newFile);
+
+      var newFile = new FS.File(newFile);
       newFile.metadata = {blogId: blogId, postId: "unknown yet", unvalid: true, last: true};
 
       var imageId = Images.insert(newFile, function (err, fileObj) {
         Session.set('imageId', imageId._id);
         Session.set('imageToAdd', imageId._id);
+
+      });
+      //console.log("newFile "+ newFile);
+
+
       });
 
 
@@ -85,6 +94,58 @@ Template.postSubmit.helpers({
     return Categories.find({blogId: this.blog._id});  
   }  
 });
+
+
+var myCanvasFunction = function(file, callback){
+  console.log("File !!!!"+file);
+
+var canvas = document.createElement("canvas");
+ // Create an image
+    var img = document.createElement("img");
+    // Create a file reader
+    var reader = new FileReader();
+    // Set the image once loaded into file reader
+
+
+console.log("DEBUG 1");
+
+       reader.onload = function (readerEvent) {
+            var image = new Image();
+console.log("DEBUG 2");
+            image.onload = function (imageEvent) {
+
+                // Resize the image
+                var canvas = document.createElement('canvas'),
+                    max_size = 100,
+                    width = image.width,
+                    height = image.height;
+                if (width > height) {
+                    if (width > max_size) {
+                        height *= max_size / width;
+                        width = max_size;
+                    }
+                } else {
+                    if (height > max_size) {
+                        width *= max_size / height;
+                        height = max_size;
+                    }
+                }
+                canvas.width = width;
+                canvas.height = height;
+                canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+                resizedImage = canvas.toDataURL('image/jpeg');
+console.log("DEBUG 3 "+dataURLToBlob(resizedImage));
+callback(dataURLToBlob(resizedImage));
+            }
+            image.src = readerEvent.target.result;
+//console.log("DEBUG 4 "+image.src);
+        }
+        reader.readAsDataURL(file);
+console.log("DEBUG 5 "+reader.readAsDataURL(file));
+    }
+
+
+
 
 Template.postSubmit.rendered = function(){
 
@@ -149,4 +210,50 @@ Template.postSubmit.rendered = function(){
     //  }
     //
     //});
+}
+
+
+/* Utility function to convert a canvas to a BLOB */
+var dataURLToBlob = function(dataURL) {
+    var BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+        var parts = dataURL.split(',');
+        var contentType = parts[0].split(':')[1];
+        var raw = parts[1];
+
+        return new Blob([raw], {type: contentType});
+    }
+
+    var parts = dataURL.split(BASE64_MARKER);
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+
+    var uInt8Array = new Uint8Array(rawLength);
+
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], {type: contentType});
+}
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
 }
