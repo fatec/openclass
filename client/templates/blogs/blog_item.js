c6223624095c498d7e2b3cd7bf6fd02d67049d0f
Vincent Widmer <eodecorp@hotmail.com>
2015-11-13 11:30:49 +0100

    Added blog member recognition

--- client/templates/blogs/blog_item.js
+++ client/templates/blogs/blog_item.js
@@ -2,5 +2,9 @@ Template.blogItem.helpers({
 	ownBlog: function() {
 		if (this.userId === Meteor.userId() || Roles.userIsInRole(Meteor.userId(), ['admin']) === true)
     		return true;
+  	},
+  	memberBlog: function() {
+  		if (typeof Blogs.findOne({_id:this._id, memberUserEmail: Meteor.user().emails[0].address}) != "undefined")
+  			return true;
   	}
 });
