43d2c7fdbd7a8d7d8b1bb69279174cd2f2a79b4a
Vincent Widmer <eodecorp@hotmail.com>
2016-02-22 16:57:01 +0100

    Test mail

--- client/templates/application/login.js
+++ client/templates/application/login.js
@@ -29,13 +29,18 @@ Template.login.events({
     },
      'click .send-mail-forgot-password': function(e) {
     e.preventDefault();
-    
-    Accounts.emailTemplates.from = "AwesomeSite Admin <accounts@example.com>";
+
+    //Accounts.emailTemplates.from = "AwesomeSite Admin <accounts@example.com>";
 
     Accounts.forgotPassword({email:'eodecorp@hotmail.com'});
     alert("Votre mot de passe vous a été envoyé par e-mail !")    
 
     }
+});
+
+Meteor.startup(function() {
+        Accounts.emailTemplates.from = "AwesomeSite Admin <accounts@example.com>";
+
 });
 
 Template.login.helpers({
