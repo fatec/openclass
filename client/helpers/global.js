b61a3182f68b1604622e6cff5264e2204eafbf5a
Vincent Widmer <eodecorp@hotmail.com>
2017-04-14 13:37:55 +0200

    Remove Google Analytics from head

--- client/helpers/global.js
+++ client/helpers/global.js
@@ -6,8 +6,4 @@ Template.registerHelper("isBox", function () {
 });
 
 Meteor.startup(function () {
-
-	// Add Google Analytics if it is web server
-	if (!Meteor.settings.public.isBox === "true")
-	    $("head").append("<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-72195513-1', 'auto');ga('send', 'pageview');</script>");
 	});
