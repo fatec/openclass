ed8d241434c184a22ac5b6b7e5816d403cb79405
Vincent Widmer <eodecorp@hotmail.com>
2017-05-01 18:41:29 +0200

    Clean code + refactoring

--- client/templates/application/desktop_menu.js
+++ client/templates/application/desktop_menu.js
@@ -1,140 +1,105 @@
-  function addClick(blogId,content) {
-
-   var authorId = Authors.findOne({name: Session.get(blogId).author});
-   console.log(authorId);
-    Authors.update({ _id: authorId._id },{ $push: { clicks: content }})
-  }
-
-  
 Template.desktopMenu.helpers({
 
-	postCount: function() { // return the number of posts
+	postCount: function() {
 		return this.posts.count();
 	},
-	favoritesCount: function() {
-		return Posts.find({favorites: true}).fetch().length;
+	authors: function() {
+		return Authors.find({}, {sort: {name: 1}});
+	},
+	authorNRef: function() {
+		return Posts.find({author:this.name}).fetch().length;
+	},
+	categories: function() {
+		return Categories.find({}, {sort: {name: 1}});
+	},
+	categoriesNRef: function() {
+		return Posts.find({category:this.name}).fetch().length;
 	},
 	tags: function() {
 		return Tags.find({}, {sort: {name: 1}});
-	},
-	categories: function() {
-		return Categories.find({ nRefs: { $gt: 0 } }, {sort: {name: 1}});
 	},	
-	tagQuery: function() {
-		return "tags="+this.name;
+	tagsNRef: function() {
+		return Posts.find({tags:{$in : [this.name]}}).fetch().length;
 	},
-	categoryQuery: function() {
-		return "category="+this.name;
-	},	
 	'selectedTagClass': function(){
 		var tagId = this.name;
-	    if (Session.get('filter') === 'tag')
-	    {
-	      var selectedTag = Session.get('tag');
-	      if(tagId == selectedTag){
-	        return "menu--list-element-selected"
-	      }
-    	}
+		if (Session.get('filter') === 'tag') {
+			var selectedTag = Session.get('tag');
+			if(tagId == selectedTag)
+				return "desktop-menu--list-element-selected"
+		}
 	},
-	'selectedCategoryClass': function(){
+	'selectedCategoryClass': function() {
 		var categoryId = this.name;
-		if (Session.get('filter') === 'category')
-	    {
-	      var selectedCategory = Session.get('category');
-	      if(categoryId == selectedCategory){
-	        return "menu--list-element-selected"
-	      }
-    	}
+		if (Session.get('filter') === 'category') {
+			var selectedCategory = Session.get('category');
+			if(categoryId == selectedCategory)
+				return "desktop-menu--list-element-selected"
+		}
 	},	
-	'selectedAuthorClass': function(){
+	'selectedAuthorClass': function() {
 		var authorId = this.name;
-		if (Session.get('filter') === 'author')
-	    {
-	      var selectedAuthor = Session.get('author');
-	      if(authorId == selectedAuthor){
-	        return "menu--list-element-selected"
-	      }
-    	}
+		if (Session.get('filter') === 'author') {
+			var selectedAuthor = Session.get('author');
+			if(authorId == selectedAuthor)
+				return "desktop-menu--list-element-selected"
+		}
 	},		
-	'selectedAllPostsClass': function(){
-	    var sortPosts = Session.get('sortPosts');
-	    if(sortPosts == "all"){
-	        return "menu--list-element-selected"
-	    }
+	'selectedShowAll': function() {
+		var sortPosts = Session.get('sortPosts');
+		if (Session.get('filter'))
+			if (Session.get('filter') != '')
+				return false
+			else return "desktop-menu--list-element-selected"
+				else return "desktop-menu--list-element-selected"	
 	},
-	'selectedLastPostsClass': function(){
-    	var sortPosts = Session.get('sortPosts');
-    	if (Session.get('filter'))
-    		if (Session.get('filter') != '')
-    			return false
-    		else return "menu--list-element-selected"
-    	else return "menu--list-element-selected"	
-    	
-	},
-	'selectedFavoritesClass': function(){
-    	if (Session.get('filter') === 'favorites')
-    		return "menu--list-element-selected"
-    	else return false;
-    	
-	},	
-  'isDisabledClass': function() {
-    if (this.nRefs == 0)
-      return "menu--tag-disabled"
-    else return null
-  },
-	authors: function() {
-		return Authors.find({ nRefs: { $gt: 0 } }, {sort: {name: 1}});
-	},
-	authorQuery: function() {
-		return "author="+this.name;
+	'isDisabled': function(nRef) {
+		if (nRef == 0)
+			return "desktop-menu--list-element-disabled"
+		else return null
 	}
+	// 	'selectedFavoritesClass': function(){
+	// 		if (Session.get('filter') === 'favorites')
+	// 			return "desktop-menu--list-element-selected"
+	// 		else return false;
+	// },	
+	// favoritesCount: function(){
+	// 	return Posts.find({favorites: true}).fetch().length;
+	// },
 });
 
-  Template.desktopMenu.events({
-  'click .menu--link-last-posts': function(e,template) {
-    Session.set("filter", ""); 
-    Session.set('nbPosts',Posts.find({}).fetch().length); 
-    Session.set("click", Session.get("click")+1);
-    addClick(template.data.blog._id,"view all");
-  },
-    'click .menu--link-favorites': function(e,template) {
-    Session.set("filter", "favorites"); 
-    Session.set('nbPosts',Posts.find({favorites: true}).fetch().length); 
-
-    //Session.set('posts',Posts.find({favorites:1}, {sort: {nb: 1}}).fetch()); 
-    Session.set("click", Session.get("click")+1);
-    addClick(template.data.blog._id,"view favorites");
-  },
-  	  'click .filter-tag': function(e,template) {
-    e.preventDefault();
-    Session.set('filter','tag');
-    var tag = $(e.target).data('tag');
-    Session.set('tag',tag);
-    Session.set('nbPosts',Posts.find({tags: tag}).fetch().length); 
-    Session.set("click", Session.get("click")+1);
-    addClick(template.data.blog._id,"tag: "+tag);
-  },
-  'click .filter-author': function(e,template) {
-    e.preventDefault();
-    Session.set('filter','author');
-    var author = $(e.target).data('author');
-    Session.set('author',author);
-    //Session.set('posts',Posts.find({author: author}, {sort: {nb: 1}}).fetch()); 
-    Session.set('nbPosts',Posts.find({author: author}).fetch().length); 
-    Session.set("click", Session.get("click")+1);
-    addClick(template.data.blog._id,"author: "+author);
-  },
-  'click .filter-category': function(e,template) {
-    e.preventDefault();
-    Session.set('filter','category');
-    var category = $(e.target).data('category');
-    Session.set('category',category);
-    //Session.set('posts',Posts.find({category: category}, {sort: {nb: 1}}).fetch()); 
-        Session.set('nbPosts',Posts.find({category: category}).fetch().length); 
-
-    Session.set("click", Session.get("click")+1);
-    addClick(template.data.blog._id,"category: "+category);
-     //Session.set('nbPosts', Posts.find({}).fetch().length);
-
-  }  
+Template.desktopMenu.events({
+	
+	'click .desktop-menu--show-all': function(e) {
+		e.preventDefault();
+		Session.set("filter", ""); 
+		Session.set('nbPosts',Posts.find({}).fetch().length); 
+	},
+	'click .filter-tag': function(e) {
+		e.preventDefault();
+		Session.set('filter','tag');
+		var tag = $(e.target).data('tag');
+		Session.set('tag',tag);
+		Session.set('nbPosts',Posts.find({tags: tag}).fetch().length); 
+	},
+	'click .filter-author': function(e) {
+		e.preventDefault();
+		Session.set('filter','author');
+		var author = $(e.target).data('author');
+		Session.set('author',author);
+		Session.set('nbPosts',Posts.find({author: author}).fetch().length); 
+	},
+	'click .filter-category': function(e) {
+		e.preventDefault();
+		Session.set('filter','category');
+		var category = $(e.target).data('category');
+		Session.set('category',category);
+		Session.set('nbPosts',Posts.find({category: category}).fetch().length); 
+	} 
+	// 'click .desktop-menu--link-favorites': function(e,template) {
+	// 	Session.set("filter", "favorites"); 
+	// 	Session.set('nbPosts',Posts.find({favorites: true}).fetch().length); 
+	// 	Session.set("click", Session.get("click")+1);
+	// 	addClick(template.data.blog._id,"view favorites");
+	// }, 
 });
--- client/templates/application/mobile_menu.js
+++ client/templates/application/mobile_menu.js
@@ -1,157 +1,117 @@
-  function addClick(blogId,content) {
+Template.slideoutMenu.helpers({
 
-   var authorId = Authors.findOne({name: Session.get(blogId).author});
-   console.log(authorId);
-    Authors.update({ _id: authorId._id },{ $push: { clicks: content }})
-  }
-
-
-Template.mobileMenu.helpers({
 	postCount: function() { // return the number of posts
 		if (this.posts)
 			return this.posts.count();
 	},
-	favoritesCount: function() {
-		return Posts.find({favorites: true}).fetch().length;
+	authors: function() {
+		return Authors.find({}, {sort: {name: 1}});
+	},
+	authorNRef: function() {
+		return Posts.find({author:this.name}).fetch().length;
 	},
 	tags: function() {
 		return Tags.find({}, {sort: {name: 1}});
 	},
-	categories: function() {
-		return Categories.find({ nRefs: { $gt: 0 } }, {sort: {name: 1}});
-	},	
-	tagQuery: function() {
-		return "tags="+this.name;
+	tagsNRef: function() {
+		return Posts.find({tags:{$in : [this.name]}}).fetch().length;
 	},
-	categoryQuery: function() {
-		return "category="+this.name;
+	categories: function() {
+		return Categories.find({}, {sort: {name: 1}});
 	},	
+	categoriesNRef: function() {
+		return Posts.find({category:this.name}).fetch().length;
+	},
+	'selectedAuthorClass': function(){
+		var authorId = this.name;
+		if (Session.get('filter') === 'author')
+		{
+			var selectedAuthor = Session.get('author');
+			if (authorId == selectedAuthor)
+				return "slideout-menu--list-element-selected"
+		}
+	},
 	'selectedTagClass': function(){
 		var tagId = this.name;
-	    if (Session.get('filter') === 'tag')
-	    {
-	      var selectedTag = Session.get('tag');
-	      if(tagId == selectedTag){
-	        return "mobile-menu--list-element-selected"
-	      }
-    	}
+		if (Session.get('filter') === 'tag')
+		{
+			var selectedTag = Session.get('tag');
+			if (tagId == selectedTag)
+				return "slideout-menu--list-element-selected"
+		}
 	},
 	'selectedCategoryClass': function(){
 		var categoryId = this.name;
 		if (Session.get('filter') === 'category')
-	    {
-	      var selectedCategory = Session.get('category');
-	      if(categoryId == selectedCategory){
-	        return "mobile-menu--list-element-selected"
-	      }
-    	}
+		{
+			var selectedCategory = Session.get('category');
+			if (categoryId == selectedCategory)
+				return "slideout-menu--list-element-selected"
+		}
 	},	
-	'selectedAuthorClass': function(){
-		var authorId = this.name;
-		if (Session.get('filter') === 'author')
-	    {
-	      var selectedAuthor = Session.get('author');
-	      if(authorId == selectedAuthor){
-	        return "mobile-menu--list-element-selected"
-	      }
-    	}
-	},		
-	'selectedAllPostsClass': function(){
-	    var sortPosts = Session.get('sortPosts');
-	    if(sortPosts == "all"){
-	        return "mobile-menu--list-element-selected"
-	    }
-	},
-	'selectedLastPostsClass': function(){
-    	var sortPosts = Session.get('sortPosts');
-    	if (Session.get('filter'))
-    		if (Session.get('filter') != '')
-    			return false
-    		else return "mobile-menu--list-element-selected"
-    	else return "mobile-menu--list-element-selected"	
-    	
+	'selectedShowAll': function(){
+		var sortPosts = Session.get('sortPosts');
+		if (Session.get('filter'))
+			if (Session.get('filter') != '')
+				return false
+			else return "slideout-menu--list-element-selected"
+				else return "slideout-menu--list-element-selected"	
 	},	
-		'selectedFavoritesClass': function(){
-    	if (Session.get('filter') === 'favorites')
-    		return "mobile-menu--list-element-selected"
-    	else return false;
-    	
+	'isDisabled': function(nRef) {
+		if (nRef == 0)
+			return "slideout-menu--list-element-disabled"
+		else return null
 	},
-	'isDisabledClass': function() {
-    if (this.nRefs == 0)
-      return "menu--tag-disabled"
-    else return null
-  },
-	authors: function() {
-		return Authors.find({ nRefs: { $gt: 0 } }, {sort: {name: 1}});
-	},
-		authorName: function() {
-		return Session.get(this.blog._id).author; 
-	},    
-	authorQuery: function() {
-		return "author="+this.name;
-	},	
-	isReactive: function() {
-		return Session.get('isReactive');
-    }
+	ownBlog: function() {
+		if (this.blog.userId === Meteor.userId() || Roles.userIsInRole(Meteor.userId(), ['admin']) === true)    		return true;
+  	},   	
+	// favoritesCount: function() {
+	// 	return Posts.find({favorites: true}).fetch().length;
+	// },	
+	// 'selectedFavoritesClass': function(){
+	// 	if (Session.get('filter') === 'favorites')
+	// 		return "mobile-menu--list-element-selected"
+	// 	else return false;
+	// },
 });
 
-Template.mobileMenu.events({
-  'click .menu--link-last-posts': function(e,template) {
-    Session.set("filter", ""); 
-    Session.set('nbPosts',Posts.find({}).fetch().length); 
-    Session.set("click", Session.get("click")+1);
-    // addClick(template.data.blog._id,"view all");
-  },
-      'click .menu--link-favorites': function(e,template) {
-    Session.set("filter", "favorites"); 
-    Session.set('nbPosts',Posts.find({favorites: true}).fetch().length); 
+Template.slideoutMenu.events({
 
-    //Session.set('posts',Posts.find({favorites:1}, {sort: {nb: 1}}).fetch()); 
-    Session.set("click", Session.get("click")+1);
-        // addClick(template.data.blog._id,"view favorites");
-  },
-	'click .mobile-menu--button-close-wrapper': function(e){
+	'click .slideout-menu--link-show-all': function(e) {
+	Session.set("filter", ""); 
+	Session.set('nbPosts',Posts.find({}).fetch().length); 
+	},
+	'click .slideout-menu--exit': function(e){
 		e.preventDefault();
-		slideout.close();   
+		Router.go('blogsList');
 	},
-	'click .mobile-menu--button-back-wrapper': function(e){
-	e.preventDefault();
-      Router.go('blogsList');
+	'click .slideout-menu--settings': function(e,template){
+		e.preventDefault();
+		Router.go('blogEdit',{_id: template.data.blog._id});
 	},
-	'click .mobile-menu--button-change-user': function(e,template){
-	e.preventDefault();
-      Router.go('blogUsers',{_id: template.data.blog._id});
-  },
-  	'click .mobile-menu--button-settings': function(e,template){
-	e.preventDefault();
-      Router.go('blogEdit',{_id: template.data.blog._id});
-  },
-   'click .filter-tag': function(e,template) {
-    e.preventDefault();
-    Session.set('filter','tag');
-    var tag = $(e.target).data('tag');
-    Session.set('tag',tag);
-    Session.set('nbPosts',Posts.find({tags: tag}).fetch().length); 
-    // Session.set("click", Session.get("click")+1);
-    // addClick(template.data.blog._id,"tag: "+tag);
-  },
-  'click .filter-author': function(e,template) {
-    e.preventDefault();
-    Session.set('filter','author');
-    var author = $(e.target).data('author');
-    Session.set('author',author);
-    Session.set('nbPosts',Posts.find({author: author}).fetch().length); 
-    // Session.set("click", Session.get("click")+1);
-    // addClick(template.data.blog._id,"author: "+author);
-  },
-  'click .filter-category': function(e,template) {
-    e.preventDefault();
-    Session.set('filter','category');
-    var category = $(e.target).data('category');
-    Session.set('category',category);
-        Session.set('nbPosts',Posts.find({category: category}).fetch().length); 
-    // Session.set("click", Session.get("click")+1);
-    // addClick(template.data.blog._id,"category: "+category);
-  }     
+	'click .filter-author': function(e) {
+		e.preventDefault();
+		Session.set('filter','author');
+		var author = $(e.target).data('author');
+		Session.set('author',author);
+		Session.set('nbPosts',Posts.find({author: author}).fetch().length); 
+	},	
+	'click .filter-category': function(e) {
+		e.preventDefault();
+		Session.set('filter','category');
+		var category = $(e.target).data('category');
+		Session.set('category',category);
+		Session.set('nbPosts',Posts.find({category: category}).fetch().length); 
+	}, 	
+	'click .filter-tag': function(e) {
+		e.preventDefault();
+		Session.set('filter','tag');
+		var tag = $(e.target).data('tag');
+		Session.set('tag',tag);
+		Session.set('nbPosts',Posts.find({tags: tag}).fetch().length); 
+	}	
+	// 'click .menu--link-favorites': function(e,template) {
+	// Session.set("filter", "favorites"); 
+	// Session.set('nbPosts',Posts.find({favorites: true}).fetch().length); 
+	// },
 });
