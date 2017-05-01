47852a9073db860225fbb718d58ed6f4c6ed5713
Vincent Widmer <eodecorp@hotmail.com>
2017-05-01 18:42:08 +0200

    Add alert message when disconnected

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,56 +1,81 @@
-  Template.layout.onRendered(function () {
-    var template = this;
-    slideout = new Slideout({
-      'panel': template.$('#content').get(0),
-      'menu': template.$('#slideout-menu').get(0),
-      'padding': 256,
-      'tolerance': 70,
-      'touch': false
-    });
-        T9n.setLanguage("fr");
+Template.layout.onRendered(function () {
 
+	// Slideout (lateral menu)
+	var template = this;
+	slideout = new Slideout({
+		'panel': template.$('#content').get(0),
+		'menu': template.$('#slideout-menu').get(0),
+		'padding': 256,
+		'tolerance': 70,
+		'touch': false
+	});
 
+	function closeMenu(event) {
+		event.preventDefault();
+		slideout.close();
+		event.stopPropagation(); // Prevent click propagation to menu wrapper button
+	}
 
+	// Close lateral menu on click
+	slideout
+	.on('beforeopen', function() {
+		this.panel.classList.add('panel-open');
+	})
+	.on('open', function() {
+		this.panel.addEventListener('click', closeMenu);
+	})
+	.on('beforeclose', function() {
+		this.panel.classList.remove('panel-open');
+		this.panel.removeEventListener('click', closeMenu);
+	});
 
-  function close(event) {
-  event.preventDefault();
-  slideout.close();
-  event.stopPropagation(); // Prevent click propagation to menu wrapper button
-}
+	T9n.setLanguage("fr");
 
-slideout
-  .on('beforeopen', function() {
-    this.panel.classList.add('panel-open');
-  })
-  .on('open', function() {
-    this.panel.addEventListener('click', close);
-  })
-  .on('beforeclose', function() {
-    this.panel.classList.remove('panel-open');
-    this.panel.removeEventListener('click', close);
-  });
-
-  
-
-  });
-
-  Template.layout.events({
-  // Speedup focus on input for mobile devices
-  'touchend input': function(e) {
-    $(e.target).focus();
-  },
-  'touchend textarea': function(e) {
-    $(e.target).focus();
-  },  
-    'click .header--button-menu-wrapper': function(e) {
-        e.preventDefault();
-        slideout.toggle();
+Meteor.autorun(function () {
+    var stat;
+    if (Meteor.status().status === "connected") {
+        stat = 1;
     }
+    else if (Meteor.status().status === "connecting") {
+        stat = 0;
+    }
+    else {
+        stat = 0;
+    }
+    Session.set('status',stat);
 });
 
-  Template.layout.helpers({
-  isAdmin: function() {
-    if (Roles.userIsInRole(Meteor.userId(), ['admin']) === true)
-        return true;
-    }
+});
+
+
+Template.layout.events({
+	
+	'touchend input': function(e) { // Speedup focus on input for mobile devices
+		$(e.target).focus();
+	},
+	'touchend textarea': function(e) {
+		$(e.target).focus();
+	},  
+	'click .header--button-menu-wrapper': function(e) {
+		e.preventDefault();
+		slideout.toggle();
+	}
+});
+
+Template.layout.helpers({
+	
+	isAdmin: function() {
+		if (Roles.userIsInRole(Meteor.userId(), ['admin']) === true)
+			return true;
+	},
+	status: function() {
+		console.log(Session.get('status'));
+		if (!Session.get('status'))
+			return 'layout--status-disconnected'
+		else
+			return 'layout--status-connected'
+	},
+	 isBox: function() { // Check if Meteor is running under a beekee box
+    return (Meteor.settings.public.isBox === "true")
+  },
 });
