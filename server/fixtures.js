4da2928a761342690c1ab73ace0de00bac114322
Vincent Widmer <eodecorp@hotmail.com>
2017-06-26 15:22:15 +0200

    Revert "Change admin email in fixtures"
    This reverts commit 2e870a400ff7c089a0ed6fcee036c50ccaa41e13.

--- server/fixtures.js
+++ server/fixtures.js
@@ -12,7 +12,7 @@ if (Blogs.find().count() === 0) {
 			var id;
 			id = Accounts.createUser({
 				username: user.username,
-				email: "admin@admin.com",
+				email: "Admin",
 				password: adminPassword
 			});
 
